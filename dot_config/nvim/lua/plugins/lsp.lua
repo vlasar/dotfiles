0ab1549686ead7333da70de1a13a2a977e5a9d9a
Vladimir Sarić <vladimir@vladimirsaric.com>
2022-02-04 18:24:38 +0100

    Add LSP progress plugin

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -5,6 +5,7 @@ plugin = {
   requires = {
     {
       'neovim/nvim-lspconfig',
+      'j-hui/fidget.nvim'
       --ft = {'ruby'}
     }
   },
@@ -12,11 +13,17 @@ plugin = {
   config = function()
     local null_ls = require("null-ls")
     local nvim_lsp = require("lspconfig")
+    local fidget = require("fidget")
+
+    fidget.setup({})
 
     null_ls.setup({
       sources = {
+        --null_ls.builtins.code_actions.eslint_d,
         null_ls.builtins.code_actions.gitsigns,
+        --null_ls.builtins.diagnostics.eslint_d,
         null_ls.builtins.diagnostics.rubocop,
+        --null_ls.builtins.formatting.eslint_d,
         null_ls.builtins.formatting.rubocop,
       }
     })
