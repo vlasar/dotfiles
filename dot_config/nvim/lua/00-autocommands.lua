4774463d3b7dae0105b0586ea793040c5272f626
Vladimir Sarić <vladimir@vladimirsaric.com>
2022-04-17 12:39:50 +0200

    Update autocommands

--- dot_config/nvim/lua/00-autocommands.lua
+++ dot_config/nvim/lua/00-autocommands.lua
@@ -1,8 +1,7 @@
 vim.cmd[[
-  " Show cursor line only in the active window
-  au InsertLeave,WinEnter * set cursorline
-  au InsertEnter,WinLeave * set nocursorline
-
   " Highlight on yank
   au TextYankPost * lua vim.highlight.on_yank {}
+
+  " set all `es6` extensions to javascript
+  au BufRead,BufNewFile *.{es6,es6.js} set filetype=javascript
 ]]
